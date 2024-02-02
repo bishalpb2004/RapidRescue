2b8054c98700975dcd000abae71a462e4cfb3c7e
Parthiv Kumar Das <130219761+parthiv002@users.noreply.github.com>
2024-02-02 13:53:13 +0530

    Home added

--- app/src/main/java/com/example/rapidrescue/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/rapidrescue/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Delete"
     }
     val text: LiveData<String> = _text
 }
