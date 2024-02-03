dc3230a4b2fa82d081efb91c61440da7cbfca80b
Domestikus <turinteron@gmail.com>
2024-02-03 11:54:30 +0530

    added conditions to upload data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("org.koin:koin-android-viewmodel:3.2.0")
+    implementation ("org.koin:koin-android-scope:3.2.0")
+    implementation ("org.koin:koin-android:3.2.0")
+
 }
