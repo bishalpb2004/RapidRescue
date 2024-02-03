d3f11259821e1365e842ab1f08fb15ae3f2985bb
Parthiv Kumar Das <130219761+parthiv002@users.noreply.github.com>
2024-02-03 12:30:13 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation ("org.koin:koin-android-viewmodel:3.2.0")
-    implementation ("org.koin:koin-android-scope:3.2.0")
-    implementation ("org.koin:koin-android:3.2.0")
-
 }
