9aa36849094befa485f0467d21c8a882564fbd10
Parthiv Kumar Das <130219761+parthiv002@users.noreply.github.com>
2024-04-17 17:55:46 +0530

    splashcreen ui fixes

--- app/src/main/java/com/example/rapidrescue/splash_screen.kt
+++ app/src/main/java/com/example/rapidrescue/splash_screen.kt
@@ -3,6 +3,7 @@ package com.example.rapidrescue
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +13,13 @@ class splash_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        window?.apply {
+            // Add FLAG_FULLSCREEN to the window flags
+            addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+
+
         setContentView(R.layout.activity_splash_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
