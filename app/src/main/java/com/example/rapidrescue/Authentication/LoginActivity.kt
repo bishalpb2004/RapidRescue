4147cbce963a4277e19be0a52a5d7a89c20965fc
Parthiv Kumar Das <130219761+parthiv002@users.noreply.github.com>
2024-07-12 18:50:41 +0530

    phone authentication remove + email & password authentication applied

---
+++ app/src/main/java/com/example/rapidrescue/Authentication/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.rapidrescue.Authentication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rapidrescue.R
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        }
+    }
