440668429b7707d16209c5859bffaf10f41c78c2
Bishal Paul Biswas <bishalp.biswas@gmail.com>
2024-08-02 23:08:20 +0530

    optimisation done to a level

--- app/src/main/java/com/example/rapidrescue/developers.kt
+++ app/src/main/java/com/example/rapidrescue/developers.kt
@@ -6,45 +6,60 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.rapidrescue.databinding.FragmentDevelopersBinding
-import com.example.rapidrescue.ui.BottomSheetDialogs.BottomSheet_Bishal
-import com.example.rapidrescue.ui.BottomSheetDialogs.BottomSheet_Parthiv
-import com.example.rapidrescue.ui.BottomSheetDialogs.BottomSheet_Rajdeep
-import com.example.rapidrescue.ui.BottomSheetDialogs.BottomSheet_Turin
-
+import com.example.rapidrescue.ui.BottomSheetDialogs.BottomSheetDeveloper
 
 class developers : Fragment() {
-    private lateinit var binding:FragmentDevelopersBinding
+    private lateinit var binding: FragmentDevelopersBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        binding= FragmentDevelopersBinding.inflate(inflater,container,false)
+        binding = FragmentDevelopersBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val bottomSheetParthiv = BottomSheet_Parthiv()
-        val bottomsheetBishal=BottomSheet_Bishal()
-        val bottomsheetTurin=BottomSheet_Turin()
-        val bottomsheetRajdeep=BottomSheet_Rajdeep()
-
         binding.btnParthiv.setOnClickListener {
-            bottomSheetParthiv.show(childFragmentManager,"BottomSheetDialog")
+            val bottomSheet = BottomSheetDeveloper.newInstance(
+                "https://github.com/parthiv002",
+                "https://www.linkedin.com/in/parthiv-kumar-das-9b5b87257/",
+                "https://www.instagram.com/parthivkumardas/",
+                "https://www.facebook.com/profile.php?id=100087913660839"
+            )
+            bottomSheet.show(childFragmentManager, "BottomSheetDialog")
         }
 
         binding.btnBishal.setOnClickListener {
-            bottomsheetBishal.show(childFragmentManager,"BottomSheetDialog")
+            val bottomSheet = BottomSheetDeveloper.newInstance(
+                "https://github.com/bishalpb2004",
+                "https://www.linkedin.com/in/bishal-paul-biswas-592341257/",
+                "https://www.instagram.com/bishalbiswas2004/",
+                "https://www.facebook.com/profile.php?id=100087658158317"
+            )
+            bottomSheet.show(childFragmentManager, "BottomSheetDialog")
         }
 
         binding.btnTurin.setOnClickListener {
-            bottomsheetTurin.show(childFragmentManager,"BottomSheetDialog")
+            val bottomSheet = BottomSheetDeveloper.newInstance(
+                "https://github.com/Domestikus",
+                "https://www.linkedin.com/in/turin-teron-369459259/",
+                "https://www.instagram.com/turrrinn/",
+                "https://www.facebook.com/profile.php?id=100087960463029"
+            )
+            bottomSheet.show(childFragmentManager, "BottomSheetDialog")
         }
 
         binding.btnRajdeep.setOnClickListener {
-            bottomsheetRajdeep.show(childFragmentManager,"BottomSheetDialog")
+            val bottomSheet = BottomSheetDeveloper.newInstance(
+                "https://github.com/Rajdeepsark",
+                "https://www.linkedin.com/in/rajdeep-sarkar-00b80a244/",
+                "https://www.instagram.com/rajdeeps_arkar/",
+                "https://www.facebook.com/freaky.Rajdeep"
+            )
+            bottomSheet.show(childFragmentManager, "BottomSheetDialog")
         }
     }
 }
---
+++ app/src/main/java/com/example/rapidrescue/ui/BottomSheetDialogs/BottomSheetDeveloper.kt
@@ -0,0 +1,75 @@
+package com.example.rapidrescue.ui.BottomSheetDialogs
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rapidrescue.databinding.FragmentBottomSheetDeveloperBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetDeveloper : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentBottomSheetDeveloperBinding
+
+    companion object {
+        private const val ARG_GITHUB = "github"
+        private const val ARG_LINKEDIN = "linkedin"
+        private const val ARG_INSTAGRAM = "instagram"
+        private const val ARG_FACEBOOK = "facebook"
+
+        fun newInstance(github: String, linkedin: String, instagram: String, facebook: String): BottomSheetDeveloper {
+            val fragment = BottomSheetDeveloper()
+            val args = Bundle().apply {
+                putString(ARG_GITHUB, github)
+                putString(ARG_LINKEDIN, linkedin)
+                putString(ARG_INSTAGRAM, instagram)
+                putString(ARG_FACEBOOK, facebook)
+            }
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBottomSheetDeveloperBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val githubUrl = arguments?.getString(ARG_GITHUB)
+        val linkedinUrl = arguments?.getString(ARG_LINKEDIN)
+        val instagramUrl = arguments?.getString(ARG_INSTAGRAM)
+        val facebookUrl = arguments?.getString(ARG_FACEBOOK)
+
+        binding.gitIcon.setOnClickListener {
+            openUrl(githubUrl)
+        }
+
+        binding.linkedInIcon.setOnClickListener {
+            openUrl(linkedinUrl)
+        }
+
+        binding.instaIcon.setOnClickListener {
+            openUrl(instagramUrl)
+        }
+
+        binding.fbIcon.setOnClickListener {
+            openUrl(facebookUrl)
+        }
+    }
+
+    private fun openUrl(url: String?) {
+        url?.let {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(it)
+            startActivity(intent)
+        }
+    }
+}
